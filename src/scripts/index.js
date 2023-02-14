d3c188811e29b56db76d516d41fb2ce47c3d0f78
Noverry Ambo <noverryambo@gmail.com>
2023-02-14 10:58:43 +0700

    add for pwa

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,16 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/mobile.css';
+import App from "./views/app";
+
+
+
+const app = new App({
+    button: document.querySelector('#hamburgerButton'),
+    drawer: document.querySelector('#navigationDrawer'),
+    content: document.querySelector('#mainContent'),
+});
+
 
 // Fetch data json
 import ('../DATA.json').then(({default: jsonData}) => {
